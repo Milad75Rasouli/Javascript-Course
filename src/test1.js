fcbad75956253644562af35e264a1993f08d1a75
Milad Rasouli <milad75rasouli@gmail.com>
2023-03-18 21:31:47 +0400

    easy variable types

--- src/test1.js
+++ src/test1.js
@@ -1,2 +1,18 @@
 // this a comment.
 //console.log('Hello world from external javascrip.')
+
+// it was usually used in old versions of javascript.
+var name = "Milad";
+
+let number = 1122;
+//let number = 3322; // it's not allowed
+
+const ID = 2;
+// ID = 22; // it's not allowed
+
+let result = ID + number;
+console.log(number);
+console.log(result);
+
+
+
