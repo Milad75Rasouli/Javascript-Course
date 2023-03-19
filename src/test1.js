5bf103c696172a958faf7dbdb1c6431af9e51fab
Milad Rasouli <milad75rasouli@gmail.com>
2023-03-19 18:39:03 +0400

    a simple if condition

--- src/test1.js
+++ src/test1.js
@@ -1,39 +1,18 @@
-// loops
+// conditions
 
-const products = [
-    {
-        ID: 1,
-        title: 'Car 1',
-        price: 20,
-        exists: true
-    },
-    {
-        ID: 2,
-        title: 'Car 2',
-        price: 90,
-        exists: false
-    }
-]
+const price = 100
+const newPrice = 99
 
-// soulution 1
-console.log('Soulution 1')
-for(let item=0;
-    item< products.length;
-    item++){
-    const {title} = products[item]    
-    console.log(`#${item} title is ${title}`);
+// Note: Javascript is a 
+// case-sensitive language
+if(newPrice > price){
+    console.log('Discount has been added.')
 }
-
-// soulution 2
-console.log('Soulution 2')
-for(let item=0;
-    item< products.length;
-    item++){
-    // the below is wrong cuz, I used array index format
-    // for an object indexing
-    // const title = products[item][title]    
-    const title = products[item].title    
-    console.log(`#${item} title is ${title}`);
+else if(newPrice<price){
+    console.log(`New Price is ${newPrice}`)
+}
+else{
+    console.log('It is equal.')
 }
 
 
