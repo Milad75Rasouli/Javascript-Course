53811e2b36154c471a3fe9f71c4df0e85078202d
Milad Rasouli <milad75rasouli@gmail.com>
2023-03-19 20:26:21 +0400

    Arrow Functions

--- src/test1.js
+++ src/test1.js
@@ -1,19 +1,16 @@
 // Functions
 
-//             _________
-// inputs --->| Function| ---> outputs
+//             __________
+// inputs --->| Function | ---> outputs
 // a good program was made by a lot of
 // functions. 
 
-
-function addProduct(title= "Book 0", 
-    price= 10){
-    // it's not necessery to declar the
-    // result as let it can decuct it
-    let result =  `Title: ${title}\
-     - Price: ${price}`
-    return result
-}
+// The new feature of Javascrip is 
+// *** Arrow Functions ***
+const addProduct = (title= "Book 0", 
+    price= 10)=> `Title: ${title} - Price: ${price}`
+    
+// Even we don't need {}, return
 
 conclution = addProduct("Book 2")
 console.log(conclution)
