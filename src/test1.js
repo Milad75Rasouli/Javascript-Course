368b27715823375d04c4a2fc999b47ac384ff26b
Milad Rasouli <milad75rasouli@gmail.com>
2023-03-19 13:28:47 +0400

    a simple loop

--- src/test1.js
+++ src/test1.js
@@ -1,27 +1,25 @@
-// JSON => Javascript Object Notaition.
-const products = [
-    {
-        ID: 1,
-        title: 'Car 1',
-        price: 20,
-        exists: true
-    },
-    {
-        ID: 2,
-        title: 'Car 2',
-        price: 90,
-        exists: false
-    }
-]
+// loops
 
-// convert the variable to a JSON 
-// string of products
-const data = JSON.stringify(products)
-console.log(data);
+// const products = [
+//     {
+//         ID: 1,
+//         title: 'Car 1',
+//         price: 20,
+//         exists: true
+//     },
+//     {
+//         ID: 2,
+//         title: 'Car 2',
+//         price: 90,
+//         exists: false
+//     }
+// ]
 
-// convert a JSON string to an object
-const response = JSON.parse(data)
+// we normaly use loops to presse on
+// some datas or variable.
+for(let item=0; item<10; item++){
+    console.log(`item is ${item}`);
+}
 
-console.log(response);
 
 
