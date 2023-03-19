bd3bcb3b132ee1100667a4e3ef35c419a9e90e0a
Milad Rasouli <milad75rasouli@gmail.com>
2023-03-19 13:40:33 +0400

    an advance type of loop

--- src/test1.js
+++ src/test1.js
@@ -1,24 +1,39 @@
 // loops
 
-// const products = [
-//     {
-//         ID: 1,
-//         title: 'Car 1',
-//         price: 20,
-//         exists: true
-//     },
-//     {
-//         ID: 2,
-//         title: 'Car 2',
-//         price: 90,
-//         exists: false
-//     }
-// ]
+const products = [
+    {
+        ID: 1,
+        title: 'Car 1',
+        price: 20,
+        exists: true
+    },
+    {
+        ID: 2,
+        title: 'Car 2',
+        price: 90,
+        exists: false
+    }
+]
 
-// we normaly use loops to presse on
-// some datas or variable.
-for(let item=0; item<10; item++){
-    console.log(`item is ${item}`);
+// soulution 1
+console.log('Soulution 1')
+for(let item=0;
+    item< products.length;
+    item++){
+    const {title} = products[item]    
+    console.log(`#${item} title is ${title}`);
+}
+
+// soulution 2
+console.log('Soulution 2')
+for(let item=0;
+    item< products.length;
+    item++){
+    // the below is wrong cuz, I used array index format
+    // for an object indexing
+    // const title = products[item][title]    
+    const title = products[item].title    
+    console.log(`#${item} title is ${title}`);
 }
 
 
