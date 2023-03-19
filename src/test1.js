aa53df5f8007e3c60158289738f6aad30c4c2a48
Milad Rasouli <milad75rasouli@gmail.com>
2023-03-19 13:11:21 +0400

    array and object

--- src/test1.js
+++ src/test1.js
@@ -1,18 +1,30 @@
-// objects
+// arrays and objects
 
-//const product = ['Book', 99, true, 'description:']
+// It's the best use-case of 
+// array and object.
 
-const product = {
-    title: "Book",
-    price: 20,
+const products = [
+    {
+        ID: 1,
+        title: 'Car 1',
+        price: 20,
+        exists: true
+    },
+    {
+        ID: 2,
+        title: 'Car 2',
+        price: 90,
+        exists: false
+    }
+]
+
+products.push({
+    ID:3,
+    title: 'Car 3',
+    price: 80,
     exists: true
-}
+})
 
-// destructuring in map/ dictinary
-const {price} = product
-
-console.log(product.title);
-console.log(price);
-
+console.log(products);
 
 
