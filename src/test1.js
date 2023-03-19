275e6894f5cffbf2798ceac4b9385218a561ec9c
Milad Rasouli <milad75rasouli@gmail.com>
2023-03-19 11:08:02 +0400

    array destructuring

--- src/test1.js
+++ src/test1.js
@@ -1,21 +1,11 @@
-const title = 'BOOK'
-const price = 200
-const exists = true
+//arrays
 
-// It's a const array. 
-// Note: in const we can't reassign an index,
-// but we can add a new data.
-const product = ['Book', 99, true]
+const product = ['Book', 99, true, 'description:']
 
-// product[1] = 22 
-// product[3] = 'add index 3'
-// //product = [] // It's totaly forbanded.
-// product.push('product 1') // add at the end
-// product.unshift("product 2") // add at the beginig
-// product.pop()
+// destructuring in javascript
+const [title,price, ...others] = product
+ 
 
-
-console.log(product.length);
-console.log(product);
-
+console.log(title, price);
+console.log(others)
 
