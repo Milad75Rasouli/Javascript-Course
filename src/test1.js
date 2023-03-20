087b758ceb54a720f873bf3ebf587e46701944df
Milad Rasouli <milad75rasouli@gmail.com>
2023-03-20 13:14:45 +0400

    all types of classes

--- src/test1.js
+++ src/test1.js
@@ -9,7 +9,7 @@
 
 // Note: we alway declar constructor
 // function in Pascal form.
-function Product(title, price){
+function Product1(title, price){
     // when we use "this" this means
     // it's a property for Product.
     this.title = title
@@ -27,7 +27,26 @@ function Product(title, price){
         Price:${this.price}`
 }
 
-let good = new Product("Book 1", 444)
-result = good.productInfo2()
-console.log(good)
+let good1 = new Product1("Book 1", 444)
+result = good1.productInfo2()
+console.log(good1)
 console.log(result)
+
+// classs 2
+// it's been recomended to use this
+// format to have a class in javascript
+console.log("Class 2")
+class Product2{
+    constructor(title, price){
+        this.title = title
+        this.price = price
+    }
+    productInfo(){
+        return `Title:${this.title}\
+        Price:${this.price}`
+    }
+}
+const good2 = new Product2("Plate 1", 22)
+const conclution = good2.productInfo()
+console.log(good2)
+console.log(conclution)
